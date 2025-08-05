a4c400b21b716ea09f4ca7d0425d6cd6c29d329d
warioddly <warioddly@gmail.com>
2025-08-05 22:13:22 +0600

    first commit

---
+++ whispr/whisprApp.swift
@@ -0,0 +1,17 @@
+//
+//  whisprApp.swift
+//  whispr
+//
+//  Created by GØDØFIMØ on 5/8/25.
+//
+
+import SwiftUI
+
+@main
+struct whisprApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
